582a0387ace0c34453ddb83bbc8f48a8b5f590f2
Yashas P Nolamba <yashaspnolamba@gmail.com>
2026-08-22 11:40:24 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,70 @@
+
+// // import Card from './card.jsx';
+// // import Student from './student.jsx';
+// // import Usergreet from "./usergreet";
+
+
+
+// //  import Profile from "./profile";
+
+// // import Product from "./product";
+
+// // import Notify from "./notify";
+
+//   return(
+//     <>
+// {/* <Card></Card> */}
+// {/* <Student name="Tim" age={19} IsStudent={true}></Student>
+//  <Student name="Alex" age={17} IsStudent={true}></Student>
+//  <Student name="Steve" age={21} IsStudent={true}></Student> */}
+
+// {/* 
+//  <Usergreet IsLoggedin ={false} username = "Yashas P"></Usergreet> */}
+
+//  {/* <Profile Name="Steve" Age={25} IsOnline={false}></Profile> */}
+
+
+// </> 
+//   );
+
+// const cars_list = [{id:1,name: "BMW",speed: 190 },
+//                 {id:2,name: "Mercedes",speed: 195},
+//                 {id:3,name: "Audi",speed:145},
+//                 {id:4,name: "Volkwageon",speed:135}];
+//     return(<>
+//        {cars_list.length > 0 && <List items={cars_list} category="Cars"></List> } 
+//         </>
+//     );
+//  import Users from "./users.jsx";
+
+
+
+    //     const list_products =[{id:1,name:"watch",price:100},
+    //         {id:2,name:"pendrive",price:200},
+    //         {id:3,name:"mobile",price:850},
+    //         {id:4,name:"clothes",price:1000}];
+    
+    // return(
+    //     <>
+    //     <Users prods={list_products} category="Dig Products"></Users>
+    //     </>
+    // );
+
+    // import Button from "./button.jsx";
+
+    // import Onchange from "./onchange.jsx" ;
+
+    // import Pwcheck from "./pwchecker.jsx"
+
+    // import Regapp from "./regapp.jsx"
+
+    // import ArrUpdate from "./arrupdate.jsx"
+
+    // import Objarr from "./Objarr.jsx"
+
+import SignUp from "./SignUp.jsx"
+
+function App(){
+    return(<SignUp/>);
+}
+export default App
